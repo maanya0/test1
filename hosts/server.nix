b805217e6116ea951f5d014964545ee747a38c3f
Maanya Patel <maanyapatel145@gmail.com>
2025-12-07 14:38:13 +0530

    Update server.nix

--- hosts/server.nix
+++ hosts/server.nix
@@ -8,6 +8,8 @@ in
 {
   # Garnix server setup
   garnix.server.enable = true;
+  garnix.server.persistence.enable = true;
+  garnix.server.persistence.name = "server154";
 
   # Recommended: set system.stateVersion for reproducible behaviour
   system.stateVersion = "24.05";

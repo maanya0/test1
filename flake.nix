713335d8dda96bfac34100fccd201c42d812956a
maanya145 <maanya145@gmail.com>
2025-12-09 14:06:00 +0530

    fix server deployment

--- flake.nix
+++ flake.nix
@@ -3,11 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    garnix-lib.url = "github:garnix-io/garnix-lib";
+    garnix-lib = {
+      url = "github:garnix-io/garnix-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, garnix-lib, ... }: {
     nixosConfigurations.server = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
         garnix-lib.nixosModules.garnix
         ./hosts/server.nix
